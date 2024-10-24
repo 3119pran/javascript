55c2c08de75b50ac3e72b5e80b70f2cfdce0d0ec
Mittapalli Praneeth <111076668+3119pran@users.noreply.github.com>
2024-10-24 15:22:20 +0000

    second commit

--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,18 @@
-console.log("praneeth")
+var name="Praneeth"
+let city="hyderabad"
+const roll="22R01A66G5"
+surname="mittapalli"
+let college;
+/*
+not use the var
+because the have issue in block scope and functional scope
+*/
+name="Ravi"
+city="karimnagar"
+surname="Mittapalli"
+console.log([name,city,roll,surname,college]);
+
+/* 
+output
+[ 'Ravi', 'karimnagar', '22R01A66G5', 'Mittapalli', undefined ]
+ */
